abe268f4f27c74fbdb9f4cced11cb04d0b931b68
zhanglailiang <zhanglailiang@lvmama.com>
2019-04-08 09:55:28 +0800

    first commit

---
+++ src/main/java/com/microservice/study/MicroserviceStudyApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (MicroserviceStudyApplication.class, args);
+    }
+
+}
